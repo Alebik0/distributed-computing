fe70b731e0f7fd4d55c43f158623719fbf5dbd6e
Artem Zhmurov <zhmurov@gmail.com>
2025-09-10 13:42:53 +0300

    Initial import

---
+++ 2.Factorial/Factorial/README.md
@@ -0,0 +1,3 @@
+# Factorial of a number n
+
+Writa a function that computes a factorial of a given number n
---
+++ 2.Factorial/Factorial/factorial.py
@@ -0,0 +1,5 @@
+def factorial(n: int) -> int:
+    """
+    Calculate the factorial of a number.
+    """
+    # TODO: Your solution
